5452f8ffe2b79a416da54aa5ecd38d919355501a
AlexOLary <AlexOLary@gmail.com>
2021-12-04 21:54:56 +0700

    error handling

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,16 @@
+class DeviceController {
+    async registration (req, res){
+
+    }
+    async login (req, res){
+
+    }
+    async check (req, res){
+
+    }
+    async getOne (req, res){
+
+    }
+}
+
+module.exports = new DeviceController()
