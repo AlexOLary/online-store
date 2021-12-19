461165ed6841aa27146ecee7a4ed9de5a3b7cf5e
AlexOLary <AlexOLary@gmail.com>
2021-12-20 00:15:03 +0700

    Registration, authorization, jwt token, bcrypt

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -31,11 +31,11 @@ class UserController {
         const {email, password} = req.body
         const user = await User.findOne({where: {email}})
         if (!user) {
-            return next(ApiError.internal('Пользователь не найден'))
+            return next(ApiError.badRequest('Пользователь не найден'))
         }
         let comparePassword = bcrypt.compareSync(password, user.password)
         if (!comparePassword) {
-            return next(ApiError.internal('Указан неверный пароль'))
+            return next(ApiError.badRequest('Указан неверный пароль'))
         }
         const token = generateJwt(user.id, user.email, user.role)
         return res.json({token})
